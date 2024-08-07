a9430c46d987a0cd68629f540aa4b04af3563f1e
Semen Semenov <ssmvn77@gmail.com>
2024-08-07 23:24:42 +0300

    Update my print
    Add function print_flt

--- my_print.py
+++ my_print.py
@@ -10,11 +10,19 @@ def print_int(number):
     else:
         print(f"{number} is not integer")
 
+def print_flt(flt):
+    if isinstance(flt, float):
+        print(flt)
+    else:
+        print(f"{flt} is not float")
+
 
 if __name__ == "__main__":
     print_text("Hello Fucing World!")
     print_text(125)
-    print("\n")
+    print("-------")
     print_int(258)
     print_int("Hello")
-
+    print("-------")
+    print_flt(5.26)
+    print_flt(5)
