8bd0369708365efdb5dacdbc7e8285c15883c911
Semen Semenov <ssmvn77@gmail.com>
2024-08-08 22:32:40 +0300

    Update input text
    Optimize input_text function

--- my_input.py
+++ my_input.py
@@ -2,11 +2,8 @@ LINE=5
 
 def input_text():
     inputTxt = input("Enter any string: ")
-    if isinstance(inputTxt, str):
-        print(f"You input: {inputTxt}")
-    else:
-        print(f"{inputTxt} is not string")
+    print(f"You input: {inputTxt}")
+
 
 if __name__ == "__main__":
     input_text()
-    
